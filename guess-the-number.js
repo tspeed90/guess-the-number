de1c586852be3e740d3caa92c534cb2ed1834a64
tspeed90 <tspeed90@gmail.com>
2017-11-25 19:13:49 +0000

    simple version of a Guess the Number game with messages to indicate how close player is to generated number

---
+++ guess-the-number.js
@@ -0,0 +1,30 @@
+const gameNumber = Math.floor(Math.random() * 100 + 1);
+let playerGuess;
+let guessField = document.querySelector(".guess-field");
+let messages = document.querySelector(".messages");
+let guessButton = document.querySelector("button");
+
+function checkPlayerGuess() {
+    playerGuess = guessField.value;
+    playerGuess = parseInt(playerGuess, 10);
+    if (playerGuess === gameNumber) {
+      messages.innerText = "Congratulations!\nYou guessed it!";
+    }
+
+    if (playerGuess !== gameNumber) {
+      let difference = Math.abs(gameNumber - playerGuess);
+      if (difference <= 3) {
+        messages.innerText = "Great guess! Very close!";
+      } else if (difference <= 10) {
+        messages.innerText = "Good guess. You're not far off!";
+      } else {
+        messages.innerText = "Ooo! Try again!";
+      }
+    }
+  }
+guessButton.addEventListener('click', checkPlayerGuess);
+guessField.addEventListener('keypress', function(e) {
+  if (e.keyCode === 13) {
+    checkPlayerGuess();
+  }
+});
