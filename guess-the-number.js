9cc5aabf8fc90b9dc1e3f0c12027685802adfc94
tspeed90 <tspeed90@gmail.com>
2017-11-26 12:36:06 +0000

    added even or odd hint and an area to display hint message

--- guess-the-number.js
+++ guess-the-number.js
@@ -1,8 +1,11 @@
 const gameNumber = Math.floor(Math.random() * 100 + 1);
-let playerGuess;
+let playerGuess = -1;
 let guessField = document.querySelector(".guess-field");
 let messages = document.querySelector(".messages");
-let guessButton = document.querySelector("button");
+let guessButton = document.querySelector(".submit-btn");
+let hintButton = document.querySelector(".hint-btn");
+let hintMessage = document.querySelector(".hint-message");
+
 
 function checkPlayerGuess() {
     playerGuess = guessField.value;
@@ -10,13 +13,12 @@ function checkPlayerGuess() {
     if (playerGuess === gameNumber) {
       messages.innerText = "Congratulations! You guessed it!";
     }
-
     if (playerGuess !== gameNumber) {
       let difference = Math.abs(gameNumber - playerGuess);
       if (difference <= 3) {
         messages.innerText = "Great guess! Very close!";
       } else if (difference <= 10) {
-        messages.innerText = "Good guess. You're not far off!";
+        messages.innerText = "Good guess. Getting closer!";
       } else {
         messages.innerText = "Ooo! Try again!";
       }
@@ -26,5 +28,15 @@ guessButton.addEventListener('click', checkPlayerGuess);
 guessField.addEventListener('keypress', function(e) {
   if (e.keyCode === 13) {
     checkPlayerGuess();
+  }
+});
+function checkEvenOrOdd() {
+  hintMessage.innerText = gameNumber % 2 === 0  ?  "The number is even." : "The number is odd.";
+}
+hintButton.addEventListener('click', function() {
+  if (playerGuess === -1) {
+    hintMessage.innerText = "Sorry, I can't give you any hints until you've made a guess."
+  } else {
+    checkEvenOrOdd();
   }
 });
