df03696453b9cb2b195725b9c1b6372f293dd413
tspeed90 <tspeed90@gmail.com>
2019-01-30 14:29:16 +0000

    fixes checkDivisibleBy bug causing hint to be skipped

--- guess-the-number.js
+++ guess-the-number.js
@@ -14,13 +14,18 @@ let hintList = [
   checkHigherThan50,
   giveOneDigit
 ];
-hintCount.textContent = '0 / 5';
 
+hintCount.textContent = '0';
 guessField.focus();
 
 if (gameNumber < 10) {
   let giveDigitIndex = hintList.indexOf(giveOneDigit);
-  hintList.splice(giveDigitIndex);
+  hintList.splice(giveDigitIndex, 1);
+}
+
+if (checkDivisibleBy() === undefined) {
+  let divisibleByIndex = hintList.indexOf(checkDivisibleBy);
+  hintList.splice(divisibleByIndex, 1);
 }
 
 function displayAfterWin() {
@@ -29,10 +34,11 @@ function displayAfterWin() {
       ? 'Congratulations!\n You guessed it in ' + guessCount + ' guess!'
       : 'Congratulations!\n You guessed it in ' + guessCount + ' guesses!';
   guessField.style.display = 'none';
-  hintButton.style.display = 'none';
+  hintButton.textContent = 'Play Again!';
   guessButton.innerText = 'Play Again';
-  guessButton.removeEventListener('click', checkPlayerGuess);
-  guessButton.addEventListener('click', function() {
+  guessButton.removeAttribute('hidden');
+  hintButton.removeEventListener('click', checkPlayerGuess);
+  hintButton.addEventListener('click', function() {
     window.location.reload();
   });
 }
@@ -57,41 +63,55 @@ function checkPlayerGuess() {
 }
 
 guessButton.addEventListener('click', checkPlayerGuess);
+guessField.addEventListener('keydown', function(e) {
+  if (e.key === 'Enter') {
+    checkPlayerGuess();
+  }
+});
 
+function displayHint(hint) {
+  hintMessage.innerText = hint;
+}
+
+// --- HINTS ---
 function checkEvenOrOdd() {
-  hintMessage.innerText =
-    gameNumber % 2 === 0 ? 'The number is even.' : 'The number is odd.';
+  return gameNumber % 2 === 0 ? 'The number is even.' : 'The number is odd.';
 }
+
 function compareLastGuess() {
-  hintMessage.innerText =
-    playerGuess > gameNumber
-      ? 'The number is lower than your guess of ' + playerGuess + '.'
-      : 'The number is higher than your guess of ' + playerGuess + '.';
+  return playerGuess > gameNumber
+    ? 'The number is lower than your guess of ' + playerGuess + '.'
+    : 'The number is higher than your guess of ' + playerGuess + '.';
 }
+
 function checkDivisibleBy() {
   if (gameNumber % 5 === 0) {
-    hintMessage.innerText = 'The number is divisible by 5.';
+    return 'The number is divisible by 5.';
   } else if (gameNumber % 6 === 0) {
-    hintMessage.innerText = 'The number is divisible by 6.';
+    return 'The number is divisible by 6.';
   } else if (gameNumber % 7 === 0) {
-    hintMessage.innerText = 'The number is divisible by 7.';
+    return 'The number is divisible by 7.';
   } else if (gameNumber % 8 === 0) {
-    hintMessage.innerText = 'The number is divisible by 8.';
+    return 'The number is divisible by 8.';
   } else if (gameNumber % 9 === 0) {
-    hintMessage.innerText = 'The number is divisible by 9.';
+    return 'The number is divisible by 9.';
   }
+  return undefined;
 }
+
 function checkHigherThan50() {
-  hintMessage.innerText =
-    gameNumber >= 50
-      ? 'The number is greater than or equal to 50.'
-      : 'The number is less than 50.';
+  return gameNumber >= 50
+    ? 'The number is greater than or equal to 50.'
+    : 'The number is less than 50.';
 }
+
 function giveOneDigit() {
+  if (gameNumber < 10) {
+    return undefined;
+  }
   let gameNumberString = gameNumber.toString();
   let gameNumberDigit = gameNumberString[Math.floor(Math.random() * 2)];
-  hintMessage.innerText =
-    'One of the digits from the number is ' + gameNumberDigit + '.';
+  return 'One of the digits from the number is ' + gameNumberDigit + '.';
 }
 
 let usedHints = [];
@@ -105,19 +125,21 @@ hintButton.addEventListener('click', function() {
     while (usedHints.includes(randomIndex)) {
       randomIndex = Math.floor(Math.random() * hintList.length);
     }
-    hintList[randomIndex]();
+    const hintSelected = hintList[randomIndex];
+    displayHint(hintSelected());
     usedHints.push(randomIndex);
-    hintCount.textContent = usedHints.length + '/5';
+    hintCount.textContent = usedHints.length;
+    console.log(hintList);
   }
   if (usedHints.length >= hintList.length) {
     usedHints = [];
   }
-  hideHintButton();
 });
 
-//TODO: split into two functions? one to check for remaining hints and one to do DOM manipulation
-function hideHintButton() {
-  if (hintCount.textContent === '5/5') {
-    hintButton.style.display = 'none';
-  }
-}
+module.exports = {
+  checkEvenOrOdd,
+  checkDivisibleBy,
+  checkHigherThan50,
+  giveOneDigit,
+  compareLastGuess
+};
